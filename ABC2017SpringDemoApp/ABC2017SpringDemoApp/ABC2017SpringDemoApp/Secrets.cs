285b803804e1f71586bf93b2d6ff40f7feb29309
Kazuki Ota <kaota@microsoft.com>
2017-05-25 11:03:39 +0900

    キー情報などを書くファイルを追加

---
+++ ABC2017SpringDemoApp/ABC2017SpringDemoApp/ABC2017SpringDemoApp/Secrets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC2017SpringDemoApp
+{
+    static class Secrets
+    {
+        public const string TwitterConsumerKey = "";
+        public const string TwitterConsumerSecret = "";
+        public const string TwitterAccessToken = "";
+        public const string TwitterAccessTokenSecret = "";
+
+        public const string CognitiveServiceComputerVisionAPIKey = "";
+    }
+}
