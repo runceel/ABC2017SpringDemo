e764acbc4850ac524ad28a63045c2f2e94b46df7
Kazuki Ota <kaota@microsoft.com>
2017-05-25 10:44:45 +0900

    使用ライブラリを参照に追加と.NET Standard化

---
+++ ABC2017SpringDemoApp/ABC2017SpringDemoApp/ABC2017SpringDemoApp/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ABC2017SpringDemoApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
